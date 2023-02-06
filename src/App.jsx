8873b9f11746e1665649b60573acfc8b07e5d2cc
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-06 13:59:04 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className='bg-bleached-cedar-main h-screen w-screen'>
+        <h1 className='text-center text-monza-main text-4xl font-bold'>Portfolio de Timothée Brindejonc</h1>
+    </div>
+  );
+}
+
+export default App
