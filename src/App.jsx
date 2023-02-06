af1d24de3b60fb2eb232176ca3a0438e9aa8fe61
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-06 16:15:42 +0100

    feat: add About section in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import About from './components/About';
+
 function App() {
 
-  return (
-    <div className='bg-bleached-cedar-main h-screen w-screen'>
-        <h1 className='text-center text-monza-main text-4xl font-bold'>Portfolio de Timothée Brindejonc</h1>
-    </div>
-  );
+    return (
+        <div className='bg-bleached-cedar-main h-screen w-screen'>
+            <h1 className='text-center text-monza-main text-4xl font-bold'>Portfolio de Timothée Brindejonc</h1>
+            <About/>
+        </div>
+    );
 }
 
 export default App
