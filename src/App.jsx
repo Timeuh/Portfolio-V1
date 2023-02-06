df058a7da8c77970ce0982e257ac12c79a2cb27d
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-06 16:29:21 +0100

    style: center content

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import About from './components/About';
 function App() {
 
     return (
-        <div className='bg-bleached-cedar-main h-screen w-screen'>
+        <div className='bg-bleached-cedar-main h-screen w-screen flex flex-col justify-center'>
             <h1 className='text-center text-monza-main text-4xl font-bold'>Portfolio de Timothée Brindejonc</h1>
             <About/>
         </div>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,10 @@ import image from '../../public/icon.png';
 function About() {
 
     return (
-        <>
+        <div className='text-center flex flex-lig justify-center items-center self-center h-5/6 w-5/6'>
             <p className='text-fuel-yellow-main'>Description</p>
             <img src={image} alt='image' className='w-24 h-24'/>
-        </>
+        </div>
     );
 }
 
