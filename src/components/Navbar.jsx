b35f96665d236c84bf02e0c548be013bd450c142
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-08 13:56:46 +0100

    feat: complete navbar with dummy links for now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,11 @@ function Navbar() {
         <div id="navbar">
             <h1>Timothée Brindejonc</h1>
             <h1>Étudiant 2ème année de BUT Informatique</h1>
+            <a href="#intro">Intro</a>
             <a href="#about">A propos</a>
+            <a href="#skills">Compétences</a>
+            <a href="#projects">Projets</a>
+            <a href="#contact">Contact</a>
         </div>
     );
 }
