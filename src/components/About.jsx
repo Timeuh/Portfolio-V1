c1cfe8410818f737b28bd2f627d4b07f72c6cc6b
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-08 13:33:52 +0100

    feat: add id to section for navbar direct navigation

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import image from '../../public/icon.png';
 function About() {
 
     return (
-        <div>
+        <div id="about">
             <p>Description</p>
             <img src={image} alt='image' width="50" height="50"/>
         </div>
