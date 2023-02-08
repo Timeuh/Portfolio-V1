1921d450e4264129135d6682a148b940e3ad64d0
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-08 13:34:16 +0100

    feat: add a navbar for different sections

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar() {
+
+    return (
+        <div id="navbar">
+            <h1>Timothée Brindejonc</h1>
+            <h1>Étudiant 2ème année de BUT Informatique</h1>
+            <a href="#about">A propos</a>
+        </div>
+    );
+}
+
+export default Navbar;
