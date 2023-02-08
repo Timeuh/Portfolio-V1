905adea24037ca474c7b92fb2e7a8e6976003302
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-08 13:36:30 +0100

    feat: add navbar to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import About from './components/About';
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
 
     return (
         <div>
+            <Navbar/>
             <h1>Portfolio de Timothée Brindejonc</h1>
             <About/>
         </div>
