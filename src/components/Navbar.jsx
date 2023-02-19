a4ec0924e2492684f79be57bcea97c475995fc74
Timeuh <timotdu54@gmail.com>
2023-02-19 11:44:58 +0100

    style: add style to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
+import image from "../../public/icon.png";
+
 function Navbar() {
 
     return (
-        <div id="navbar">
-            <h1>Timothée Brindejonc</h1>
-            <h1>Étudiant 2ème année de BUT Informatique</h1>
-            <a href="#intro">Intro</a>
-            <a href="#about">A propos</a>
-            <a href="#skills">Compétences</a>
-            <a href="#projects">Projets</a>
-            <a href="#contact">Contact</a>
+        <div id="navbar" className="navbar">
+            <div className="flex flex-row space-x-4 items-center">
+                <img src={image} alt='image' width="70" height="70"/>
+                <h1>Timothée Brindejonc</h1>
+            </div>
+            <div className="flex flex-row space-x-12 items-center">
+                <a href="#intro">Intro</a>
+                <a href="#about">A propos</a>
+                <a href="#skills">Compétences</a>
+                <a href="#projects">Projets</a>
+                <a href="#contact">Contact</a>
+            </div>
         </div>
     );
 }
