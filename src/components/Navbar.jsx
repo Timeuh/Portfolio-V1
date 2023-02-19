6a6f8c3907f1438daacd29e236237010f64ba4cc
Timeuh <timotdu54@gmail.com>
2023-02-19 18:56:05 +0100

    style: change color of section titles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,19 @@
-import image from "../../public/icon.png";
+import icon from "../../public/icon.png";
 
 function Navbar() {
 
     return (
         <div id="navbar" className="navbar">
             <div className="flex flex-row space-x-4 items-center">
-                <img src={image} alt='image' width="70" height="70"/>
+                <img src={icon} alt='image' width="70" height="70"/>
                 <h1>Timothée Brindejonc</h1>
             </div>
             <div className="flex flex-row space-x-12 items-center">
-                <a href="#intro" className="navbar-item">Intro</a>
-                <a href="#about" className="navbar-item">A propos</a>
-                <a href="#skills" className="navbar-item">Compétences</a>
-                <a href="#projects" className="navbar-item">Projets</a>
-                <a href="#contact" className="navbar-item">Contact</a>
+                <a href="#intro" className="navbar-item text-martinique-500">Intro</a>
+                <a href="#about" className="navbar-item text-monza-400">A propos</a>
+                <a href="#skills" className="navbar-item text-fuel-yellow-300">Compétences</a>
+                <a href="#projects" className="navbar-item text-bleached-cedar-400">Projets</a>
+                <a href="#contact" className="navbar-item text-thunder-400">Contact</a>
             </div>
         </div>
     );
