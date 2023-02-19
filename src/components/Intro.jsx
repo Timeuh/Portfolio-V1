0ad31a25fd5808e3e44ec0b045f3bfd275fe3574
Timeuh <timotdu54@gmail.com>
2023-02-19 18:56:40 +0100

    feat: add content and style for the intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,28 @@
-import image from '../../public/icon.png';
+import icon from '../../public/icon.png';
 
 function Intro() {
 
     return (
-        <div id="intro">
-            <h1>Portfolio de Timothée Brindejonc</h1>
-            <p>Description</p>
-            <img src={image} alt='image' width="50" height="50"/>
+        <div id="intro"
+             className="h-screen bg-thunder-main text-fuel-yellow-main flex flex-row-reverse justify-around items-center">
+            <div className="h-4/6 w-2/5 p-12 text-2xl">
+                <h1 className="text-3xl p-4 text-center text-monza-main">Bonjour !</h1>
+                <div className="text-left space-y-8">
+                    <p>Je suis Timothée, j'ai 20 ans et je suis étudiant en 2e année de BUT Informatique.</p>
+                    <p>Ce site est mon portfolio, il présente
+                        <a href="#about" className="text-monza-400 hover:font-bold transition"> mon parcours</a>,
+                        <a href="#skills" className="text-fuel-yellow-300 hover:font-bold transition"> mes
+                            compétences</a> et
+                        <a href="#projects" className="text-bleached-cedar-400 hover:font-bold transition"> mes
+                            réalisations</a>.
+                    </p>
+                    <p>Il est actualisé régulièrement avec mes nouvelles productions.</p>
+                    <p className="text-monza-main">Bonne lecture !</p>
+                </div>
+            </div>
+            <div className="h-4/6 w-2/6 border-2 rounded-full border-fuel-yellow-main">
+                <img src={icon} alt='image' height="650" width="650"/>
+            </div>
         </div>
     );
 }
