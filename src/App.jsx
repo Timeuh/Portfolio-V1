8ff627ebf8489a0ebe13df5417533cfb65acaf6c
Timothee Brindejonc <timotdu54@gmail.com>
2023-02-21 17:12:59 +0100

    feat: integrate about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Intro from './components/Intro.jsx';
 import Navbar from "./components/Navbar.jsx";
+import About from "./components/About.jsx";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
         <div>
             <Navbar/>
             <Intro/>
+            <About/>
         </div>
     );
 }
