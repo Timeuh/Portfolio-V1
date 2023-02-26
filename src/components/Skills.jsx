a593c2c9282f2edbf5d63b096bac5e11462e258c
Timeuh <timotdu54@gmail.com>
2023-02-26 18:47:54 +0100

    feat: add content for known tools and skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,38 @@
 function Skills() {
 
     return (
-        <div id="skills" className="bg-thunder-700 w-full h-screen">
-
+        <div id="skills" className="bg-thunder-700 w-full h-full text-center space-y-24">
+            <h1 className="text-4xl text-monza-main pt-24">Au cours de mes années d'études, j'ai pu développer diverses
+                compétences</h1>
+            <div className="h-1/4">
+                <h1 className="text-3xl py-4 text-martinique-600">Outils maîtrisés</h1>
+                <div className="flex justify-center h-full">
+                    <div className="skills-icons bg-fuel-yellow-main border-martinique-600">
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                    </div>
+                </div>
+            </div>
+            <div className="h-1/4">
+                <h1 className="text-3xl py-4 text-fuel-yellow-main">Compétences maîtrisées</h1>
+                <div className="flex justify-center h-full">
+                    <div className="skills-icons bg-monza-main border-fuel-yellow-main">
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                        <a href="#intro"><img src="../../public/icon.png" alt="icon" width="100" height="100"/></a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
