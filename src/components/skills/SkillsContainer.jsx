25964ae23c48e07b217e8198d8220b9cedaf496b
Timeuh <timotdu54@gmail.com>
2023-02-27 22:19:57 +0100

    refactor: use of props to customize skill section

--- src/components/skills/SkillsContainer.jsx
+++ src/components/skills/SkillsContainer.jsx
@@ -1,14 +1,13 @@
 import SkillsList from "./SkillsList.jsx";
-import {interestTechs} from "../../services/LinksData.js";
 
-function SkillsContainer({border, bg_from, bg_to, skill_titles}) {
+function SkillsContainer({border, bg_from, bg_to, skill_titles, techs, title}) {
 
     return (
         <div className="flex justify-center h-full">
             <div className={`h-full w-4/6  rounded-md py-8 border-2 items-center ${border}
                         bg-gradient-to-r ${bg_from} ${bg_to}`}>
-                <h1 className="text-3xl pb-4">Technologies qui m'intéressent</h1>
-                <SkillsList techs={interestTechs}/>
+                <h1 className="text-3xl pb-4">{title}</h1>
+                <SkillsList techs={techs}/>
                 <div className="justify-around flex flex-row pt-4 font-bold">
                     {skill_titles.map((title, i) => {
                         return (
