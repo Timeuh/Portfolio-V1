caffb361d2090f5b07a09b2907f3a71373081367
Timeuh <timotdu54@gmail.com>
2023-02-27 21:33:35 +0100

    feat: extract props for skills lists in a separate class

---
+++ src/services/LinksData.js
@@ -0,0 +1,34 @@
+const knownTechs = [
+    {
+        link: "https://developer.mozilla.org/fr/docs/Web/HTML",
+        src: "../../public/icons/html.png",
+        alt: "Html"
+    },
+    {
+        link: "https://developer.mozilla.org/fr/docs/Web/CSS",
+        src: "../../public/icons/css.png",
+        alt: "Css"
+    },
+    {
+        link: "https://www.javascript.com",
+        src: "../../public/icons/javascript.png",
+        alt: "Javascript"
+    },
+    {
+        link: "https://www.php.net/manual/fr/intro-whatis.php",
+        src: "../../public/icons/php.png",
+        alt: "Php"
+    },
+    {
+        link: "https://learn.microsoft.com/fr-fr/cpp/c-language/c-language-reference?view=msvc-170",
+        src: "../../public/icons/c.png",
+        alt: "C"
+    },
+    {
+        link: "https://www.java.com/fr/",
+        src: "../../public/icons/java.png",
+        alt: "Java"
+    }
+]
+
+export default knownTechs;
