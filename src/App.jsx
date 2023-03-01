75e9f8ce61e45d59d56c976a75ac458cc3f7130d
Timeuh <timotdu54@gmail.com>
2023-03-01 19:49:55 +0100

    feat: insert the projects section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Intro from './components/Intro.jsx';
 import Navbar from "./components/Navbar.jsx";
 import About from "./components/About.jsx";
 import Skills from "./components/Skills.jsx";
+import Projects from "./components/Projects.jsx";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
             <Intro/>
             <About/>
             <Skills/>
+            <Projects/>
         </>
     );
 }
