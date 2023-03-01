607b9f130e9c2efdc25053525d2157b66b596a07
Timeuh <timotdu54@gmail.com>
2023-03-01 19:49:37 +0100

    feat: add a project component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,12 @@
+function Projects() {
+
+    return (
+        <div id="projects" className="h-screen w-full flex flex-col justify-center bg-thunder-600">
+            <div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Projects;
