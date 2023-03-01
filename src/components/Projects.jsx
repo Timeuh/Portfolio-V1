b7bb9a530bbec0b598d5aa0f18f7d972ed8dab13
Timeuh <timotdu54@gmail.com>
2023-03-01 22:23:47 +0100

    feat: add first project card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,13 @@
+import ProjectCard from "./projects/ProjectCard.jsx";
+import {mastermind} from "../services/ProjectsData.js";
+import mastermindIllu from "../../public/illustrations/IconSet.png";
+
 function Projects() {
 
     return (
         <div id="projects" className="h-screen w-full flex flex-col justify-center bg-thunder-600">
             <div>
-
+                <ProjectCard techs={mastermind} image={mastermindIllu} name="Mastermind"/>
             </div>
         </div>
     );
