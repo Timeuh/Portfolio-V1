9b3f6f4aafc3b6d970e58ba78b24fba9d5e781ac
Timeuh <timotdu54@gmail.com>
2023-03-02 22:11:56 +0100

    feat: organize content for optimal display

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,19 +1,21 @@
 import {getLangageColor} from "../../services/ProjectsData.js";
 
-function ProjectCard({techs, image, name}) {
+function ProjectCard({techs, image, name, borderColor, textColor, projectLink}) {
 
     return (
-        <div>
-            <img src={image} alt={name} width="200" height="200"/>
-            <div>
+        <a href={projectLink} className={`space-y-4 bg-black bg-opacity-30 flex flex-col items-center justify-center w-1/2 h-full 
+                rounded-md ${borderColor} border-2 py-8 hover:bg-white hover:bg-opacity-20 hover:transition`}>
+            <h1 className={`text-center text-3xl ${textColor}`}>{name}</h1>
+            <img src={image} alt={name} width="300" height="300"/>
+            <div className="text-center flex-wrap mx-8">
                 {techs.map((tech, i) => {
                     let color = getLangageColor(tech);
                     return (
-                        <span key={i} className={color}>{tech}</span>
+                        <span key={i} className={`${color} p-2 m-2  rounded-md whitespace-nowrap`}>{tech}</span>
                     );
                 })}
             </div>
-        </div>
+        </a>
     );
 }
 
