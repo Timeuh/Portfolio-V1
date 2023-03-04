4a83143c31f757c78e3526c496d2ee829ce25fb1
Timeuh <timotdu54@gmail.com>
2023-03-04 18:37:11 +0100

    feat: display description on hover

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,8 +1,25 @@
-function ProjectCard({techs, image, name, borderColor, textColor, projectLink}) {
+import {useState} from "react";
+
+function ProjectCard({techs, image, name, borderColor, textColor, projectLink, description}) {
+
+    const [hover, setHover] = useState(false);
+
+    const handleEnter = () => {
+        setHover(true);
+    }
+
+    const handleExit = () => {
+        setHover(false);
+    }
 
     return (
         <a href={projectLink} className={`space-y-4 bg-black bg-opacity-30 flex flex-col items-center justify-center w-1/2 h-full 
-                rounded-md ${borderColor} border-2 py-8 hover:bg-white hover:bg-opacity-20 hover:transition`}>
+                rounded-md ${borderColor} border-2 py-8 hover:transition relative`}>
+            <div className="h-full w-full absolute hover:bg-white hover:bg-opacity-80 transition text-justify flex flex-col justify-center
+                 text-2xl p-8"
+                 onMouseEnter={handleEnter} onMouseLeave={handleExit}>
+                {hover ? description : ""}
+            </div>
             <h1 className={`text-center text-3xl ${textColor}`}>{name}</h1>
             <img src={image} alt={name} width="300" height="300"/>
             <div className="text-center flex-wrap mx-8 flex flex-row space-x-4 justify-center">
