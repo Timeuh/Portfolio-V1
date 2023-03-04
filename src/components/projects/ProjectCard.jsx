2cc8b31f8320a3807364a38438a91c9bb514f34c
Timeuh <timotdu54@gmail.com>
2023-03-04 11:03:17 +0100

    feat: change type of displayed badge for techs

--- src/components/projects/ProjectCard.jsx
+++ src/components/projects/ProjectCard.jsx
@@ -1,5 +1,3 @@
-import {getLangageColor} from "../../services/ProjectsData.js";
-
 function ProjectCard({techs, image, name, borderColor, textColor, projectLink}) {
 
     return (
@@ -9,9 +7,8 @@ function ProjectCard({techs, image, name, borderColor, textColor, projectLink}) 
             <img src={image} alt={name} width="300" height="300"/>
             <div className="text-center flex-wrap mx-8">
                 {techs.map((tech, i) => {
-                    let color = getLangageColor(tech);
                     return (
-                        <span key={i} className={`${color} p-2 m-2  rounded-md whitespace-nowrap`}>{tech}</span>
+                        <img key={i} src={tech.link} alt={tech.name}></img>
                     );
                 })}
             </div>
