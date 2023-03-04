e76d8af785fe02ab22d71e1f75a994fdb50eb02a
Timeuh <timotdu54@gmail.com>
2023-03-04 20:24:49 +0100

    feat: implement descriptions to project cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import ProjectCard from "./projects/ProjectCard.jsx";
-import {mastermind, Portfolio, spotifree, UHC_Run} from "../services/ProjectsData.js";
+import {descriptions, mastermind, Portfolio, spotifree, UHC_Run} from "../services/ProjectsData.js";
 import mastermindIllu from "../../public/projects/mastermind.png";
 import uhcRunIllu from "../../public/projects/uhcrun.jpg";
 import spotifreeIllu from "../../public/projects/spotifree.png";
@@ -12,16 +12,16 @@ function Projects() {
             <div className="grid grid-cols-2 grid-rows-2 gap-y-8 h-3/4 w-5/6 justify-items-center">
                 <ProjectCard techs={UHC_Run} image={uhcRunIllu} name="UHC Run"
                              borderColor="border-green-400" textColor="text-green-400"
-                             projectLink={"https://github.com/Timeuh/UHC-Run"}/>
+                             projectLink={"https://github.com/Timeuh/UHC-Run"} description={descriptions.UHC_Run}/>
                 <ProjectCard techs={mastermind} image={mastermindIllu} name="Mastermind"
                              borderColor="border-yellow-900" textColor="text-yellow-900"
-                             projectLink={"https://github.com/Timeuh/Mastermind"}/>
+                             projectLink={"https://github.com/Timeuh/Mastermind"} description={descriptions.Matermind}/>
                 <ProjectCard techs={spotifree} image={spotifreeIllu} name="Spotifree"
                              borderColor="border-orange-400" textColor="text-orange-400"
-                             projectLink={"https://github.com/Timeuh/Spotifree"}/>
+                             projectLink={"https://github.com/Timeuh/Spotifree"} description={descriptions.Spotifree}/>
                 <ProjectCard techs={Portfolio} image={portfolioIllu} name="Portfolio"
                              borderColor="border-purple-400" textColor="text-purple-400"
-                             projectLink={"https://github.com/Timeuh/Portfolio"}/>
+                             projectLink={"https://github.com/Timeuh/Portfolio"} description={descriptions.Portfolio}/>
             </div>
         </div>
     );
