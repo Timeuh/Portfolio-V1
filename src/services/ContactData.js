c78fbdbff241ab5c8f1f4fb3cf5cc4cffcb90d3e
Timeuh <timotdu54@gmail.com>
2023-03-05 18:58:13 +0100

    feat: add data for contact links

---
+++ src/services/ContactData.js
@@ -0,0 +1,23 @@
+import githubIcon from "../../public/icons/github_icon.png";
+import linkedinIcon from "../../public/icons/linkedin.png";
+import mailIcon from "../../public/icons/mail.png";
+
+export const contactLinks = {
+    github: {
+        icon: githubIcon,
+        link: "https://github.com/Timeuh",
+        tooltip: "Github"
+    },
+
+    linkedin: {
+        icon: linkedinIcon,
+        link: "https://www.linkedin.com/in/timothee-brindejonc/",
+        tooltip: "LinkedIn"
+    },
+
+    mail: {
+        icon: mailIcon,
+        link: "mailto:tbrindejonc@gmail.com",
+        tooltip: "Mail"
+    }
+}
