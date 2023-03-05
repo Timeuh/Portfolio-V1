55574bdcf796b42083bb753ce2b37937f55fee71
Timeuh <timotdu54@gmail.com>
2023-03-05 19:53:58 +0100

    style: add hover effect on links

--- src/components/contact/ContactLink.jsx
+++ src/components/contact/ContactLink.jsx
@@ -1,7 +1,23 @@
+import {useState} from "react";
+
 function ContactLink({contact}) {
 
+    const [isHover, setIsHover] = useState(false);
+
+    const handleEnter = () => {
+        setIsHover(true);
+    }
+
+    const handleLeave = () => {
+        setIsHover(false);
+    }
+
     return (
-        <a href={contact.link}>
+        <a href={contact.link} className="relative">
+            <div className="contact-link"
+                 onMouseEnter={handleEnter} onMouseLeave={handleLeave}>
+                {isHover ? contact.tooltip : ""}
+            </div>
             <img src={contact.icon} alt={contact.tooltip} width="200" height="200" className="rounded-full"/>
         </a>
     );
