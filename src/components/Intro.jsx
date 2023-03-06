cb1e63025091379728a97c7db3cba1305ce27f9b
Timeuh <timotdu54@gmail.com>
2023-03-06 20:08:39 +0100

    style: make intro responsive for mobiles

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,9 +3,9 @@ import profile from '../../public/illustrations/pic_front.jpg';
 function Intro() {
 
     return (
-        <div id="intro"
-             className="h-screen bg-[url('/backgrounds/intro-bg.png')] bg-cover text-fuel-yellow-main flex flex-row-reverse justify-around items-center">
-            <div className="h-4/6 w-2/5 p-12 text-2xl">
+        <div id="intro" className="h-screen 2xl:bg-[url('/backgrounds/intro-bg.png')] 2xl:bg-cover bg-thunder-main
+             text-fuel-yellow-main flex 2xl:flex-row-reverse flex-col-reverse 2xl:justify-around justify-center items-center">
+            <div className="2xl:h-4/6 2xl:w-2/5 p-12 2xl:text-2xl">
                 <h1 className="text-3xl p-4 text-center text-monza-main">Bonjour !</h1>
                 <div className="text-left space-y-8">
                     <p>Je suis Timothée, j'ai 20 ans et je suis étudiant en 2e année de BUT Informatique.</p>
@@ -20,7 +20,7 @@ function Intro() {
                     <p className="text-monza-main">Bonne lecture !</p>
                 </div>
             </div>
-            <div className="h-4/5 w-2/5 flex justify-center items-center">
+            <div className="2xl:h-4/5 w-2/5 2xl:flex justify-center items-center py-8">
                 <img src={profile} alt='me' height="400" width="400"
                      className="rounded-full border-2 border-fuel-yellow-main"/>
             </div>
