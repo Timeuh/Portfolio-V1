a4d10007e6de23e46fd581d0feffc09f82b3e3ef
Timeuh <timotdu54@gmail.com>
2023-03-06 20:49:50 +0100

    style: make contact section responsive for mobiles

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,11 +6,11 @@ function Contact() {
     return (
         <div id="contact" className="w-full h-screen bg-thunder-500 flex flex-col justify-center">
             <div className="h-4/6 flex flex-col justify-center">
-                <h1 className="text-4xl font-bold text-center py-14 text-fuel-yellow-main">Pour me contacter, vous
+                <h1 className="text-4xl font-bold text-center py-14 text-fuel-yellow-main px-4">Pour me contacter, vous
                     pouvez utiliser un de ces
                     <span className="text-monza-main"> liens</span>
                 </h1>
-                <div className="flex flex-row space-x-14 justify-center">
+                <div className="flex flex-row 2xl:space-x-14 justify-center">
                     <ContactLink contact={contactLinks.github}/>
                     <ContactLink contact={contactLinks.linkedin}/>
                     <ContactLink contact={contactLinks.mail}/>
