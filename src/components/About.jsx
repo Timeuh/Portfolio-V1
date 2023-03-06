f56fbeba2cf5da7b701df9bd60883f5637782963
Timeuh <timotdu54@gmail.com>
2023-03-06 20:22:06 +0100

    style: make about section responsive for mobiles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,12 +6,12 @@ import webapp from '../../public/illustrations/web_app.png';
 function About() {
 
     return (
-        <div id="about"
-             className="w-full bg-thunder-800 text-fuel-yellow-main text-xl h-full bg-[url('/backgrounds/about-bg.png')] bg-cover">
-            <div className="flex flex-row justify-around items-center h-1/4 py-8">
+        <div id="about" className="w-full bg-thunder-800 text-fuel-yellow-main text-xl h-full
+            2xl:bg-[url('/backgrounds/about-bg.png')] 2xl:bg-cover">
+            <div className="flex 2xl:flex-row flex-col justify-around items-center h-1/4 py-8">
                 <img src={picture} height="300" width="300" alt="me"
                      className="rounded-full border-2 border-fuel-yellow-main"/>
-                <p className="w-2/6 text-right leading-8 pl-24">
+                <p className="2xl:w-2/6 w-3/4 pt-8 leading-8 2xl:pl-24 ">
                     Je suis <span className="text-monza-main font-bold">Timothée Brindejonc</span>, et comme je l'ai dit
                     plus haut, je suis
                     étudiant en <span className="text-monza-main font-bold"> 2e année de BUT informatique</span>.
@@ -19,10 +19,10 @@ function About() {
                     J'hésite entre continuer les études avec un Master et entrer directement sur le marché du travail.
                 </p>
             </div>
-            <div className="flex flex-row-reverse justify-around items-center h-1/4 pb-12">
+            <div className="flex 2xl:flex-row-reverse flex-col justify-around items-center h-1/4 pb-12">
                 <img src={graduation} height="300" width="300" alt="graduation"
                      className="rounded-full border-2 border-monza-main"/>
-                <p className="w-2/6 my-4 leading-8">
+                <p className="2xl:w-2/6 py-8 leading-8 w-3/4">
                     Pour m'aider à décider quelle voie choisir après les études, je compte sur les différents
                     <span className="text-monza-main font-bold"> stages en entreprise</span> demandés dans le cadre du
                     BUT, ou bien sur une
@@ -32,10 +32,10 @@ function About() {
                     ou l'insertion professionnelle.
                 </p>
             </div>
-            <div className="flex flex-row justify-around items-center h-1/4 py-8">
+            <div className="flex 2xl:flex-row flex-col justify-around items-center h-1/4 py-8">
                 <img src={frameworks} height="300" width="300" alt="frameworks"
                      className="rounded-full border-2 border-monza-main"/>
-                <p className="w-2/6 text-right pt-8 leading-8 pl-24">
+                <p className="2xl:w-2/6 pt-8 leading-8 2xl:pl-24 w-3/4">
                     Sinon, d'un point de vue développement je m'oriente vers le <span
                     className="text-monza-main font-bold"> fullstack </span>
                     avec une <span className="text-monza-main font-bold"> préférence pour le front-end</span> depuis que
@@ -47,10 +47,10 @@ function About() {
                     et agréables pour les utilisateurs.
                 </p>
             </div>
-            <div className="flex flex-row-reverse justify-around items-center h-1/4 py-8">
+            <div className="flex 2xl:flex-row-reverse flex-col justify-around items-center h-1/4 py-8">
                 <img src={webapp} height="300" width="300" alt="web app"
                      className="rounded-full border-2 border-fuel-yellow-main"/>
-                <p className="w-2/6 pt-8 leading-8">
+                <p className="2xl:w-2/6 pt-8 leading-8 w-3/4">
                     C'est pour cela qu'en ce moment, j'ai pour objectif de travailler sur des
                     <span className="text-monza-main font-bold"> projets d'applications web</span> pour
                     approfondir mes compétences et mes connaissances, <span className="text-monza-main font-bold">surtout en javascript</span>.
