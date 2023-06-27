259bb3af1af48ce092b4437c9fdcad9612f1645b
Timeuh <timeuuh@gmail.com>
2023-06-27 15:02:25 +0200

    refactor : update new portfolio step

--- src/components/PortfolioProgress.jsx
+++ src/components/PortfolioProgress.jsx
@@ -1,6 +1,6 @@
-import one from '../../public/progress/1-current.png';
+import one from '../../public/progress/1-clear.png';
 import next from '../../public/progress/next.png';
-import two from '../../public/progress/2-noclear.png';
+import two from '../../public/progress/2-current.png';
 import three from '../../public/progress/3-noclear.png';
 
 export function PortfolioProgress() {
@@ -11,13 +11,13 @@ export function PortfolioProgress() {
             Celui-ci reflétera mieux mes préférences en design et montrera plus d'animations pour signifier ma progression en front-end.
             En attendant sa parution, voici sa progression actuelle :</p>
             <div className={'w-full flex flex-col items-center xl:flex-row xl:h-1/2'}>
-                <div className={'w-full h-1/5 flex flex-col items-center justify-between space-y-4 px-8 text-center text-fuel-yellow-700'}>
+                <div className={'w-full h-1/5 flex flex-col items-center justify-between space-y-4 px-8 text-center text-green-700'}>
                     <img src={one} alt="step 1"/>
                     <h1 className={'text-2xl font-bold'}>Étape 1 : Design</h1>
                     <p>Design du portfolio, à l'aide de Figma</p>
                 </div>
                 <img src={next} alt="next" className={'w-20 h-20 my-8 rotate-90 xl:rotate-0'}/>
-                <div className={'w-full h-1/5 flex flex-col items-center justify-between space-y-4 px-8 text-center text-monza-900'}>
+                <div className={'w-full h-1/5 flex flex-col items-center justify-between space-y-4 px-8 text-center text-fuel-yellow-700'}>
                     <img src={two} alt="step 1"/>
                     <h1 className={'text-2xl font-bold'}>Étape 2 : Développement</h1>
                     <p>Développement du portfolio, avec React Js, Typescript, Eslint et Tailwind CSS</p>
