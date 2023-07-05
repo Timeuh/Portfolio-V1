d96428e1419184ce841d939b07654033014c7ef7
Timeuh <timeuuh@gmail.com>
2023-07-05 09:46:25 +0200

    feat : create new portfolio header link

---
+++ src/components/NewPortfolioHeader.jsx
@@ -0,0 +1,8 @@
+export function NewPortfolioHeader() {
+    return (
+        <div id={'newPortfolio'} className={'w-full h-10 bg-green-400 flex flex-row items-center justify-center space-x-1'}>
+            <h1 className={'text-xl text-fuel-yellow-900'}>Mon nouveau portfolio est en ligne ! Disponible sur</h1>
+            <a href="https://portfolio.timeuh.live/" className={'text-xl text-martinique-main'}>ce site</a>
+        </div>
+    )
+}
